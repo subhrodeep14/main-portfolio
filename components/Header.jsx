0d58cc1a500bcb9d819818f071a1bb018ac6ad42
Subhrodeep Acharya <subhrodeepacharya14@gmail.com>
2025-02-10 18:16:54 +0530

    navber done

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import { Button } from "./ui/button"
 import Nav from "./Nav"
+import MobileNav from "./MobileNav"
 
 const Header = () => {
   return (
@@ -18,7 +19,9 @@ const Header = () => {
                 <Button>Hire me</Button>
             </Link>
             </div>
-           
+           <div className="xl:hidden">
+              <MobileNav/>
+           </div>
         </div>
     </header>
   )
