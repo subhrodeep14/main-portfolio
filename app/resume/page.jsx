5183e6bb0bd711431e0579cef7d3fef34d0795d8
Subhrodeep Acharya <subhrodeepacharya14@gmail.com>
2025-02-15 00:10:32 +0530

    resume page updated

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,4 +1,26 @@
-import React from 'react'
+import { Info } from "lucide-react"
+import { FaHtml5,FaCss3,FaJs,FaFigma,FaNodeJs,FaReact} from "react-icons/fa"
+
+import {SiTailwindcss,SiNextdotjs} from "react-icons/si"
+
+const about=[{
+  title:"About Me",
+  description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Error rem nesciunt temporibus, beatae voluptatem nulla optio, facilis consectetur minima quaerat ducimus et",
+  info:[{
+    fieldName:"Name",
+    fieldValue:"Subhrodeep Acharya"
+  },
+  {
+    fieldName:"Email",
+    fieldValue:"subhrodeepacharya14@gmail.com"
+  },{
+    fieldName:"Freelance",
+    fieldValue:"Available"
+  },{
+    fieldName:"Languages",
+    fieldValue:"English,Bengali,Hindi"
+  }]
+}]
 
 const resume = () => {
   return (
