a0dfd3de0def0321b5bb40d4ffe4f51458be6209
Subhrodeep Acharya <subhrodeepacharya14@gmail.com>
2025-05-03 01:43:44 +0530

    linkedin update

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -2,7 +2,7 @@
 import Link from "next/link"
 import {FaGithub,FaLinkedinIn,FaTwitter} from "react-icons/fa";
 const socials=[{icon:<FaGithub/> ,path:'https://github.com/subhrodeep14'},
-    {icon:<FaLinkedinIn/> ,path:'www.linkedin.com/in/subhrodeep-acharya-ba2590266'},
+    {icon:<FaLinkedinIn/> ,path:'https://www.linkedin.com/in/subhrodeep-acharya-ba2590266/'},
     {icon:<FaTwitter/> ,path:'https://x.com/Subhrodeep2003'}
 ]
 
